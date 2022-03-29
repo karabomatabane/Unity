cce73ada7f31803f4f01c57ec685d23046191bb1
Karabo Matabane <2017285772@ufs4life.ac.za>
2022-03-29 18:05:38 +0200

    Added Rotation Script

---
+++ Assets/EarthRotate.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(new Vector3
+        {
+            x = 0,
+            y = 1,
+            z = 0
+        });
+    }
+}
